6c65a50a74845be16b76bd9286c5500edea7927c
Kim Cuady <cuady.anthonykim@gmail.com>
2023-01-30 16:56:25 +0800

    Initial commit

---
+++ bin/install.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    lsb-release \
+    software-properties-common \
+    nano
+
+# Get Docker
+echo "Checking for Docker"
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Fetching docker"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+fi
+echo "We have docker"
+echo docker -v
+
+# Get docker-compose
+echo "Checking for docker-compose"
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo "Fetching docker-compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+echo "We have docker-compose"
+echo docker-compose -v
+
+docker-compose up -d
---
+++ download_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# defines the qa model
+MODEL_DIR="https://huggingface.co/sentence-transformers/paraphrase-MiniLM-L6-v2/resolve/main"
+MODEL_NAME="paraphrase-MiniLM-L6-v2"
+
+# downloads the qa model
+mkdir ${MODEL_NAME} &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/vocab.txt &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/tokenizer_config.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/tokenizer.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/special_tokens_map.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/sentence_bert_config.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/pytorch_model.bin &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/modules.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/config_sentence_transformers.json &&\
+    wget --directory-prefix=${MODEL_NAME} ${MODEL_DIR}/config.json
